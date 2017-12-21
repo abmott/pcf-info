d4eae569a63af7cb3896645ac9d6a50b66dc6228
abmott <adam.mott@csaa.com>
2017-12-21 15:05:51 -0700

    added certs files

---
+++ ci/tasks/certs/current-certs.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'aws-sdk-s3'
+
+wrkdir = Dir.pwd
+
+
+products_list = File.new("#{wrkdir}/#{ENV['PCF_ENVIRONMENT']}-current_certs.yml", "w")
+target = `uaac target #{ENV['OPSMAN_URI']}/uaa --skip-ssl-validation`
+connect = `uaac token owner get opsman #{ENV['OPSMAN_USERNAME']} -p "#{ENV['OPSMAN_PASSWORD']}" -s ""`
+context = `uaac context`
+token = context.split("access_token: ")[1].split("      token_type: bearer")[0]
+products = JSON.parse(`curl "#{ENV['OPSMAN_URI']}/api/v0/deployed/certificates?expires_within=2y" -X GET -H "Authorization: Bearer #{token}" -k -s`)
+products_list.puts "#{ENV['PCF_ENVIRONMENT'].upcase} PCF Current Certs"
+products_list.puts ".................."
+products.each do |values|
+  products_list.puts "#{values['issuer']} #{values['valid_until']} #{values['property_reference']}"
+end
+products_list.puts ".................."
+products_list.close
+
+puts ""
+File.open("#{wrkdir}/#{ENV['PCF_ENVIRONMENT']}-current_certs.yml").each do |line|
+  puts line
+end
+
+s3 = Aws::S3::Resource.new(
+  :access_key_id => "#{ENV['AWS_ACCESS_KEY']}",
+  :secret_access_key => "#{ENV['AWS_SECRET_KEY']}",
+  :region => 'us-east-1'
+)
+
+file = "#{ENV['PCF_ENVIRONMENT']}-current_certs.yml"
+bucket = 'csaa-pcf-info'
+
+name = File.basename(file)
+
+obj = s3.bucket(bucket).object(name)
+
+obj.upload_file(file)
+
+File.delete("#{wrkdir}/#{ENV['PCF_ENVIRONMENT']}-current_certs.yml") if File.exist?("#{wrkdir}/#{ENV['PCF_ENVIRONMENT']}-current_certs.yml")
