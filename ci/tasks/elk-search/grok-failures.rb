da803d4b6523c8f017e9252fc0aa9157f8b3e2a1
abmott <adam.mott@csaa.com>
2018-01-08 15:10:02 -0700

    added grok-failure

--- ci/tasks/elk-search/grok-failures.rb
+++ ci/tasks/elk-search/grok-failures.rb
@@ -1,5 +1,83 @@
 #!/usr/bin/env ruby
 require 'json'
 
-output = `curl \"https://www.google.com\"`
+
+#output = `curl \"https://www.google.com\"`
+output = `curl -XGET "http://10.91.36.101:9200/_search" -H 'Content-Type: application/json' -d'
+{
+  "size": 0,
+  "aggs": {},
+  "version": true,
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "query_string": {
+            "analyze_wildcard": true,
+            "query": "*"
+          }
+        },
+        {
+          "match_phrase": {
+            "tags": {
+              "query": "_grokparsefailure"
+            }
+          }
+        },
+        {
+          "range": {
+            "@timestamp": {
+              "gt": "now-1h"
+            }
+          }
+        }
+      ],
+      "must_not": []
+    }
+  },
+  "_source": {
+    "excludes": []
+  },
+  "highlight": {
+    "pre_tags": [
+      "@kibana-highlighted-field@"
+    ],
+    "post_tags": [
+      "@/kibana-highlighted-field@"
+    ],
+    "fields": {
+      "*": {
+        "highlight_query": {
+          "bool": {
+            "must": [
+              {
+                "query_string": {
+                  "analyze_wildcard": true,
+                  "query": "*",
+                  "all_fields": true
+                }
+              },
+              {
+                "match_phrase": {
+                  "tags": {
+                    "query": "_grokparsefailure"
+                  }
+                }
+              },
+              {
+                "range": {
+                  "@timestamp": {
+                    "gt": "now-1h"
+                  }
+                }
+              }
+            ],
+            "must_not": []
+          }
+        }
+      }
+    },
+    "fragment_size": 2147483647
+  }
+}'`
 puts output
