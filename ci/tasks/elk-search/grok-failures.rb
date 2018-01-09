03a5700abbeaca9de271f9b7e86fab1be0ad0069
abmott <adam.mott@csaa.com>
2018-01-09 11:44:35 -0700

    added grok-failure

--- ci/tasks/elk-search/grok-failures.rb
+++ ci/tasks/elk-search/grok-failures.rb
@@ -89,5 +89,6 @@ puts ".................."
 puts ".................."
 if search_grok['hits']['total'] > 0
   then puts "is a big number"
+  curl -H "Content-Type: application/json" -X POST -d '{"grok_failures":"'"#{search_grok['hits']['total']}"'"}' https://hooks.zapier.com/hooks/catch/1143336/9a003f/
 end
 #https://hooks.zapier.com/hooks/catch/1143336/9a003f/
